0d9213560acb8ddfe47b02a6b03b602afc672a2d
Grant <gejoyner28@gmail.com>
2026-01-28 12:41:03 -0800

    Setup bash, emacs, git, install script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+DOTFILES="$HOME/dotfiles"
+
+link() {
+    src="$DOTFILES/$1"
+    dst="$HOME/$2"
+
+    # if file already exists and is not a symbolic link, back it up
+    if [-e "$dst" ] && [ ! -L "$dst" ]; then
+	echo "Backing up $dst -> $dst.bak"
+	mv "$dst" "$dst.bak"
+    fi
+
+    ln -sfn "$src" "$dst"
+    echo "Linked $dst"
+}
+
+link bash/bashrc .bashrc
+link git/gitconfig .gitconfig
+link emacs/init.el .emacs.d/init.el
