d2359bb6ae65eb483de06beee337530cb63d1b7b
Grant <gejoyner28@gmail.com>
2026-01-31 00:01:01 -0800

    Updated emacs init

--- emacs/init.el
+++ emacs/init.el
@@ -5,8 +5,12 @@
 
 (set-face-attribute 'default nil :font "Iosevka Fixed Extended") ; set font
 
-(load-theme 'tango-dark) ; set theme
+(global-display-line-numbers-mode t) ; enable line number
+(column-number-mode) ; enable column number
 
+
+
+;; packages
 (require 'package)
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
@@ -23,7 +27,59 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+
+
 (use-package ivy
   :diminish
   :config
   (ivy-mode 1))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+(use-package counsel
+  :after ivy
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file))
+  :config
+  (counsel-mode 1)
+  (setq ivy-initial-inputs-alist nil))
+
+(use-package swiper
+  :after ivy
+  :bind (("C-s" . swiper)))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+(use-package doom-themes
+  :init (load-theme 'doom-badger t))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(counsel doom-themes helpful ivy-rich)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
